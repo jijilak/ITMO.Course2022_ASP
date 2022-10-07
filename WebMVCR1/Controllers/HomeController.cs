fe6b3f3a97f86f5178bff6a33b300cfb28b4e6cf
Yuliya Lakova <julijalakova@gmail.com>
2022-10-07 21:30:20 +0300

    Lab2

--- WebMVCR1/Controllers/HomeController.cs
+++ WebMVCR1/Controllers/HomeController.cs
@@ -30,10 +30,44 @@ namespace WebMVCR1.Controllers
         //    return Greeting;
         //}
         //////////////////////////////////////Lab1Ex3/////////////////////////////////
-        public string Index(string hel)
+        //public string Index(string hel)
+        //{
+        //    string Greeting = ModelClass.ModelHello() + ", " + hel;
+        //    return Greeting;
+        //}
+        //////////////////////////////////////Lab2Ex1.1/////////////////////////////////
+        //public string Index()
+        //{
+        //    string res = ExeEnum();
+        //    return res;
+        //}
+        //////////////////////////////////////Lab2Ex1.2/////////////////////////////////
+        public string Index()
         {
-            string Greeting = ModelClass.ModelHello() + ", " + hel;
-            return Greeting;
+            string res = ExeStruct();
+            return res;
+        }
+        public string ExeEnum()
+        {
+            AccountType goldAccount; AccountType platinumAccount;
+            goldAccount = AccountType.Checking; platinumAccount = AccountType.Deposit;
+
+            string res1 = String.Format("Тип банковского счета {0}", goldAccount);
+            string res2 = String.Format("Тип банковского счета {0}", platinumAccount);
+
+            string res = res1 + "<p>" + res2;
+            return res;
+        }
+        public string ExeStruct()
+        {
+            BankAccount goldBankAccount;
+
+            goldBankAccount.accType = AccountType.Checking;
+            goldBankAccount.accBal = (decimal)3200.00;
+            goldBankAccount.accNo = 123;
+
+            string res = String.Format("Номер счета {0}, баланс {1}, тип {2}", goldBankAccount.accNo, goldBankAccount.accBal, goldBankAccount.accType);
+            return res;
         }
     }
 }
---
+++ WebMVCR1/Models/StudyCsharp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVCR1.Models
+{
+    public enum AccountType
+    {
+        Checking, Deposit
+    }
+    public struct BankAccount 
+    { 
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+    }
+    public class StudyCsharp
+    {
+    }
+}
