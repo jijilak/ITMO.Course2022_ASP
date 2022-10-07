25c6fc5a3b28e466495420ad4f18ce3c8cdbf408
Yuliya Lakova <julijalakova@gmail.com>
2022-10-07 20:39:25 +0300

    Добавьте файлы проекта.

---
+++ WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMVCR1.Models;
+
+namespace WebMVCR1.Controllers
+{
+    public class MyController : Controller
+    {
+        //////////////////////////////////////Lab1Ex0/////////////////////////////////
+        //GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        //////////////////////////////////////Lab1Ex1/////////////////////////////////
+        //public string Start()
+        //{
+        //    int hour = DateTime.Now.Hour;
+        //    string Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+        //    return Greeting;
+        //}
+        //////////////////////////////////////Lab1Ex2/////////////////////////////////
+        //public string Start(string hel)
+        //{
+        //    int hour = DateTime.Now.Hour;
+        //    string Greeting = hour < 12 ? "Доброе утро" : "Добрый день" + ", " + hel;
+        //    return Greeting;
+        //}
+        //////////////////////////////////////Lab1Ex3/////////////////////////////////
+        public string Index(string hel)
+        {
+            string Greeting = ModelClass.ModelHello() + ", " + hel;
+            return Greeting;
+        }
+    }
+}
